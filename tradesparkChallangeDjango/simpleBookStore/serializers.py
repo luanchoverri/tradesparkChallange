f5645296de1fd776f0a863541d792b0e9304c224
Luciana Anchoverri <lu.anchoverri@gmail.com>
2025-01-10 18:01:40 -0300

    entrega final 2

--- tradesparkChallangeDjango/simpleBookStore/serializers.py
+++ tradesparkChallangeDjango/simpleBookStore/serializers.py
@@ -37,15 +37,15 @@ class BookSerializer(serializers.ModelSerializer):
         author_data = validated_data.pop('author')
         categories_data = validated_data.pop('categories')
 
-        # Actualizar datos del autor
-        author_instance, created = Author.objects.get_or_create(**author_data)
-        instance.author = author_instance
-
-        # Actualizar datos del libro
+        # titulo
         instance.title = validated_data.get('title', instance.title)
         instance.save()
 
-        # Actualizar categorías
+        # autor
+        author_instance, created = Author.objects.get_or_create(**author_data)
+        instance.author = author_instance
+
+        # categoria
         instance.categories.clear()
         for category_data in categories_data:
             category_instance, created = Category.objects.get_or_create(**category_data)
